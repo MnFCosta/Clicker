ffd7da53cd870978dbf3d4f61ee6c4a01acbbf33
Manoel <mnfcosta1@gmail.com>
2023-03-30 21:18:22 -0300

    Adicionando imagens na interface

--- app/src/main/java/com/example/kotlinclicker/MainActivity.kt
+++ app/src/main/java/com/example/kotlinclicker/MainActivity.kt
@@ -2,14 +2,19 @@ package com.example.kotlinclicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -42,17 +47,88 @@ fun TelaPrincipal(){
             contentDescription = "Background",
             contentScale = ContentScale.Crop,
         )
-        Box (
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+                .border(
+                    BorderStroke(2.dp, Color(0xBCB6845D)),
+                    shape = RoundedCornerShape(8.dp)
+                ),
             contentAlignment = Alignment.TopCenter
-        ){
 
-            Image(
-                painter = painterResource(id = R.drawable._1_2_minecraft_emerald),
-                contentDescription = "My Image",
+        ) {
+            Column(
                 modifier = Modifier
-                    .width(30.dp) // Set the width of the image
-                    .height(100.dp) // Set the height of the image
+                    .fillMaxWidth()
+                    .border(
+                        BorderStroke(2.dp, Color(0xFFFF1100)),
+                        shape = RoundedCornerShape(8.dp)
+                    ),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Image(
+                    painter = painterResource(R.drawable._1_2_minecraft_emerald),
+                    contentDescription = "My Image",
+                    modifier = Modifier
+                        .padding(top = 16.dp)
+                        .width(40.dp)
+                        .height(10.dp)
+                )
+            }
+
+            Box(modifier = Modifier
+                .width(45.dp)
+                .height(340.dp)
+                .padding(start = 12.dp, top = 12.dp)
+                .border(
+                    BorderStroke(2.dp, Color(0xFFFF1100)),
+                    shape = RoundedCornerShape(8.dp)
+
+                )
+                .align(Alignment.TopStart))
+            IconButton(onClick = { /*TODO*/ }) {
+                Image(
+                    painter = painterResource(R.drawable.stonepick),
+                    contentDescription = "My Image 2",
+                    modifier = Modifier
+                        .align(Alignment.TopEnd)
+                        .padding(top = 25.dp, start = 16.dp)
+                        .width(40.dp)
+                )
+            }
+            Image(
+                painter = painterResource(R.drawable.stonepick),
+                contentDescription = "My Image 2",
+                modifier = Modifier
+                    .align(Alignment.TopEnd)
+                    .padding(top = 20.dp, start = 16.dp)
+                    .width(40.dp)
             )
+            Image(
+                painter = painterResource(R.drawable.sbeve),
+                contentDescription = "My Image 2",
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(top = 20.dp, start = 18.dp)
+                    .width(20.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.big_villager_face),
+                contentDescription = "My Image 2",
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(top = 70.dp, start = 18.dp)
+                    .width(20.dp)
+            )
+            Image(
+                painter = painterResource(R.drawable.creeper),
+                contentDescription = "My Image 2",
+                modifier = Modifier
+                    .align(Alignment.TopStart)
+                    .padding(top = 120.dp, start = 18.dp)
+                    .width(20.dp)
+            )
+
         }
         Box (
             modifier = Modifier
